e7c4bdb51099cfa169785a7e76fb87232fea6a03
licolin <li_colin@126.com>
2022-05-10 17:22:36 +0800

    cal

---
+++ 6/6.06.py
@@ -0,0 +1,111 @@
+# -*- encoding: utf-8 -*-
+# Author: li_colin
+# 二叉树解析算术运算表达式
+# 对二叉树构建的表达式求值
+# for example  (3+(5*6))
+import operator
+
+
+class BinaryTree:
+    def __init__(self, val):
+        self.data = val
+        self.left = None
+        self.right = None
+
+    def insertLeft(self, val):
+        _node = BinaryTree(val)
+        if self.left:
+            tmp = self.left
+            self.left = _node
+            _node.left = tmp
+        else:
+            self.left = _node
+
+    def insertRight(self, val):
+        _node = BinaryTree(val)
+        if self.right:
+            tmp = self.right
+            self.right = _node
+            _node.right = tmp
+        else:
+            self.right = _node
+
+    def getLeft(self):
+        return self.left
+
+    def getRight(self):
+        return self.right
+
+    def setRootVal(self, val):
+        self.data = val
+
+    def getRootVal(self):
+        return self.data
+
+
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, val):
+        self.data.append(val)
+
+    def pop(self):
+        return self.data.pop()
+
+
+str_fmt = "(3+(5*6))"
+chars = [i for i in str_fmt]
+
+
+# (3+(5*6))
+def gen_tree(_chars):
+    t_stack = Stack()
+    bt = BinaryTree("")
+    t_stack.push(bt)
+    _currentTree = bt
+    for ele in _chars:
+        if ele == "(":
+            _currentTree.insertLeft("")
+            t_stack.push(_currentTree)
+            _currentTree = _currentTree.getLeft()
+        elif ele not in ("+", "-", "*", "/", ")"):
+            _currentTree.setRootVal(int(ele))
+            _currentTree = t_stack.pop()
+        elif ele in ("+", "-", "*", "/"):
+            _currentTree.setRootVal(ele)
+            _currentTree.insertRight("")
+            t_stack.push(_currentTree)
+            _currentTree = _currentTree.getRight()
+
+        elif ele == ")":
+            _currentTree = t_stack.pop()
+        else:
+            pass
+    return bt
+
+
+# print(gen_tree(_chars=chars))
+
+
+# from operator import div
+
+_add = operator.add
+_sub = operator.sub
+_mul = operator.mul
+
+op = {"+": _add, "-": _sub, "*": _mul}
+
+
+# (3+(5*6))
+
+def cal(bt):
+    if not bt:
+        return
+    if bt.getLeft() and bt.getRight():
+        return op[bt.data](cal(bt.getLeft()), cal(bt.getRight()))
+    else:
+        return bt.data
+
+
+print(cal(gen_tree(_chars=chars)))
