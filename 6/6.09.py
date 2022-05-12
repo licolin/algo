919c74ab34b7b24ee1ba7f287f927fbac1082464
licolin <li_colin@126.com>
2022-05-12 23:53:54 +0800

    bst

--- 6/6.09.py
+++ 6/6.09.py
@@ -9,19 +9,46 @@ class Node:
         self.val = val
         self.left = None
         self.right = None
+        self.parent = None
+
+    def hasLeftChild(self):
+        return self.left
+
+    def hasRightChild(self):
+        return self.right
 
 
 class BST:
     def __init__(self):
-        self.head = None
+        self.root = None
         self.current = None
 
     def insertNode(self, node):
-        if self.head is None:
-            self.head = node
-            self.current = node
+        if self.root is None:
+            self.root = node
+            node.parent = None
         else:
-            pass
+            self.put(node, self.root)
+
+    # @staticmethod
+    def put(self, node, root):
+        if node.key < root.key:
+            if root.hasLeftChild():
+                self.put(node, root.hasLeftChild())
+            else:
+                root.left = node
+                node.parent = root
+        else:
+            if root.hasRightChild():
+                self.put(node, root.hasRightChild())
+            else:
+                root.right = node
+                node.parent = root
 
 
 bst = BST()
+bst.insertNode(Node(12, 1))
+bst.insertNode(Node(8, 5))
+bst.insertNode(Node(15, 7))
+bst.insertNode(Node(19, 20))
+print(bst.root.right.right.key)
