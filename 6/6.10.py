a1fefab3628b1e328807b1a04c61037c216b0587
licolin <li_colin@126.com>
2022-05-17 22:12:40 +0800

    avl

--- 6/6.10.py
+++ 6/6.10.py
@@ -26,6 +26,35 @@ class AVL:
                 root.right = self.insert(root.right, key, val)
             root.height = self.getHeight(root)
             factor = self.get_balance(root)
+            if factor > 1 and key < root.left.key:
+                return self.r_rotation(root)
+            if factor > 1 and key > root.left.key:
+                t = self.l_rotation(root.left)
+                return self.r_rotation(root)
+            if factor < -1 and key > root.right.key:
+                return self.l_rotation(root)
+            if factor < -1 and key < root.right.key:
+                t = self.r_rotation(root.right)
+                return self.l_rotation(root)
+            return root
+
+    def r_rotation(self, root):
+        tmp = root.left
+        tmp_sub = tmp.right
+        tmp.right = root
+        root.left = tmp_sub
+        tmp.height = self.getHeight(tmp)
+        tmp.right.height = self.getHeight(tmp.right)
+        return tmp
+
+    def l_rotation(self, root):
+        tmp = root.right
+        tmp_sub = tmp.left
+        root.right = tmp_sub
+        tmp.left = root
+        tmp.height = self.getHeight(tmp)
+        tmp.left.height = self.getHeight(tmp.left)
+        return tmp
 
     def get_balance(self, root):
         return self.getHeight(root.left) - self.getHeight(root.right)
